99449810f2279d64e440a2169fc421d98af5fda8
vsoch <vsochat@stanford.edu>
2016-11-11 17:43:15 -0800

    modified:   whatisit/apps/labelinator/templates/reports/report_collection_details.html 	modified:   whatisit/settings.py

--- whatisit/settings.py
+++ whatisit/settings.py
@@ -190,7 +190,7 @@ REST_FRAMEWORK = {
 # https://docs.djangoproject.com/en/1.9/topics/i18n/
 
 LANGUAGE_CODE = 'en-us'
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Chicago'
 USE_I18N = True
 USE_L10N = True
 USE_TZ = True
