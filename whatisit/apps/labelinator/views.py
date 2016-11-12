12be5b65ee98222b705703904fd09f0870f543fa
vsoch <vsochat@stanford.edu>
2016-11-11 18:18:52 -0800

    modified:   whatisit/apps/labelinator/templates/reports/report_collection_details.html 	modified:   whatisit/apps/labelinator/views.py

--- whatisit/apps/labelinator/views.py
+++ whatisit/apps/labelinator/views.py
@@ -166,7 +166,9 @@ def view_report_collection(request,cid):
             pass
 
     # If the user has edit_permissions, we want to show him/her users that can be added
-    context["requesters"] = RequestMembership.objects.filter(collection=collection)
+    if context["edit_permission"] == True:
+        context["requesters"] = RequestMembership.objects.filter(collection=collection)
+        context["requesters_pending"] = len([x for x in context["requesters"] if x.status == "PENDING"]
     return render(request, 'reports/report_collection_details.html', context)
 
 
