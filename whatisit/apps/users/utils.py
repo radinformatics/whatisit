030a68b5184badb1531098427cd0a49c0b01ca50
vsoch <vsochat@stanford.edu>
2016-12-10 15:37:34 -0800

    bug comma

--- whatisit/apps/users/utils.py
+++ whatisit/apps/users/utils.py
@@ -3,7 +3,7 @@ from django.contrib.auth import get_user_model
 from django.contrib.auth.models import User
 from whatisit.apps.wordfish.models import (
     ReportCollection,
-    ReportSet,
+    ReportSet
 )
 from whatisit.apps.users.models import (
     Credential,
